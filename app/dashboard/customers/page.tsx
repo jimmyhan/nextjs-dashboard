403fa7eee76c78320f1988d632b5df80ae251a3a
jimmy H <jimmy.han.work@gmail.com>
2025-05-06 12:00:45 -0700

    updated customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <p>Customers Page</p>;
-}
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { Suspense } from 'react';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+   return (
+     <div className="w-full">
+       <div className="flex w-full items-center justify-between">
+         <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+       </div>
+       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+         <Search placeholder="Search invoices..." />
+       </div>
+      <Table query={query} currentPage={currentPage} />
+     </div>
+   );
+ }
