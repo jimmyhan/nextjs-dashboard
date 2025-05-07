482e535248b26a6b827bf168eca121af268fbd81
jimmy H <jimmy.han.work@gmail.com>
2025-05-07 10:33:08 -0700

    C-16 add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,11 @@
 import Table from '@/app/ui/customers/table';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 
 export default async function Page(props: {
   searchParams?: Promise<{
